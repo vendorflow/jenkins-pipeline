2de9613b84ffbee15b3e2e4f6ca4be1a51b3574d
Christopher Smith <chrylis@gmail.com>
2021-10-26 19:35:44 -0500

    Also wrap mavenGetVersion

--- vars/mavenGetVersion.groovy
+++ vars/mavenGetVersion.groovy
@@ -1,6 +1,8 @@
 String call() {
+  withMaven(mavenSettingsConfig: 'vendorflow-ci-settings-xml') {
     return sh(
       returnStdout: true,
       script: "./mvnw -q -Dexec.executable='echo' -Dexec.args='\${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.6.0:exec | tail -n 1"
     ).trim()
+  }
 }
